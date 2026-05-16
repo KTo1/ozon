603e8d8e6af054b7e7fb137c58521aba469053a1
vasiliy-borodich <vasiliy.borodich@itm.ee>
2026-05-16 18:16:29 +0700

    list

--- concurrency_patterns/WaitGroup/main.go
+++ concurrency_patterns/WaitGroup/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
+	"sync"
+	"time"
 )
 
 // Есть интерфейс waiter, требуется реализовать структуру, имплементирующую этот интерфейс, которая:
@@ -16,23 +19,62 @@ type waiter interface {
 	Run(ctx context.Context, f func(ctx context.Context) error)
 }
 
+var _ waiter = (*waitGroup)(nil)
+
 type waitGroup struct {
-	// напишите ваш код здесь
-}
-
-// Метод waitGroup.Wait() должен ожидать завершения всех задач и возвращать ошибку
-func (g *waitGroup) Wait() error {
-	// напишите ваш код здесь
-}
-
-// Метод waitGroup.Run() должен запускать функцию f с контекстом ctx
-func (g *waitGroup) Run(ctx context.Context, fn func(ctx context.Context) error) {
-	// напишите ваш код здесь
+	maxParallel int
+	mu          sync.Mutex
+	wg          sync.WaitGroup
+	sem         chan struct{}
+	errs        []error
 }
 
 // Функция newGroupWait создает новый объект waiter с заданным количеством параллельных запусков
 func newGroupWait(maxParallel int) waiter {
-	// напишите ваш код здесь
+	return &waitGroup{
+		maxParallel: maxParallel,
+		sem:         make(chan struct{}, maxParallel),
+	}
+}
+
+// Метод waitGroup.Wait() должен ожидать завершения всех задач и возвращать ошибку
+func (g *waitGroup) Wait() error {
+	g.wg.Wait()
+
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	return errors.Join(g.errs...)
+}
+
+// Метод waitGroup.Run() должен запускать функцию f с контекстом ctx
+func (g *waitGroup) Run(ctx context.Context, fn func(ctx context.Context) error) {
+	g.wg.Add(1)
+	go func() {
+		defer g.wg.Done()
+
+		select {
+		case <-ctx.Done():
+			g.AddErr(ctx.Err())
+			return
+		case g.sem <- struct{}{}:
+		}
+
+		defer func() {
+			<-g.sem
+		}()
+
+		if err := fn(ctx); err != nil {
+			g.AddErr(err)
+		}
+	}()
+}
+
+func (g *waitGroup) AddErr(err error) {
+	g.mu.Lock()
+	defer g.mu.Unlock()
+
+	g.errs = append(g.errs, err)
 }
 
 func main() {
@@ -43,7 +85,9 @@ func main() {
 	expErr2 := errors.New("got error 2")
 
 	g.Run(ctx, func(ctx context.Context) error {
-		// Ваш код здесь
+		fmt.Println("work hard 1")
+		time.Sleep(time.Second)
+
 		return nil
 	})
 
@@ -52,8 +96,10 @@ func main() {
 	})
 
 	g.Run(ctx, func(ctx context.Context) error {
-		// Ваш код здесь
-		return nil
+		fmt.Println("work hard 2")
+		time.Sleep(2 * time.Second)
+
+		return expErr1
 	})
 
 	err := g.Wait()
